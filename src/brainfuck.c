fd6173b9b746337846b67e6d030cff6c1e4bbf2e
Gaultier Delbarre <gaultiertexas@gmail.com>
2019-10-11 22:58:53 -0500

    Added .gitignore. created interpreter

---
+++ src/brainfuck.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef enum boolean {false, true} bool;
+
+int main(int argc, char **argv)
+{
+	/* Pointer */
+	const long PTRSIZE = 16384;
+	unsigned long array[PTRSIZE];
+	unsigned long *p = array;
+	unsigned long ptrNum = 0;
+	unsigned long *tempPtr = p;
+	memset(p, 0x00, PTRSIZE);
+
+	int loop[4096] = {0};
+	int *l = loop;
+	int *tempLoop = l;
+
+	/* Files */
+	FILE *f = fopen(argv[1], "r");
+	
+	bool debug = false;
+	if(argc >= 3)
+		if(argv[2][0] == 'd')
+		{
+			printf("Debugging program.\nPrints will be with integers\n-----------------\n");
+			debug = true;
+		}
+
+	/* Compile Loop */
+	char c;
+	while((c = fgetc(f)) != EOF)
+	{
+
+		switch(c)
+		{
+			case '>':
+				if(ptrNum + 1 < PTRSIZE)
+				{
+					++p;
+					++ptrNum;
+				}
+				break;
+			case '<':
+				if(ptrNum != 0)
+				{
+					--p;
+					--ptrNum;
+				}
+				break;
+			case '+':
+				++*p;
+				break;
+			case '-':
+				if(*p <= 0)
+					*p = 0;
+				else
+					--*p;
+				break;
+			case '.':
+				if(!debug)
+					printf("%c", *p);
+				else
+					printf("%d\n", *p);
+				break;
+			case ',':
+				*p = getchar();
+				break;
+			case '[':
+				++l;
+				*l = ftell(f);
+				
+				break;
+			case ']':
+				if(*p == 0)
+				{	
+					*l = 0;
+					--l;
+				}else{
+					fseek(f, *l, SEEK_SET);
+				}
+				break;
+			case '{':
+				tempPtr = p;
+				p = malloc(sizeof(long)*PTRSIZE);
+				*p = *tempPtr;
+				tempLoop = l;
+				l = malloc(sizeof(int)*4096);
+				break;
+			case '}':
+				*tempPtr = *p;
+				free(p);
+				p = tempPtr;
+				free(l);
+				l = tempLoop;
+				break;
+		}
+	}
+	printf("\n");
+	/* Close Files */
+	fclose(f);
+}	

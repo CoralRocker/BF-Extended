a4d512c22226da05f5156eb42202af60aab54d46
Gaultier Delbarre <gaultiertexas@gmail.com>
2019-10-11 23:20:27 -0500

    Fixed invalid pointer error

--- src/brainfuck.c
+++ src/brainfuck.c
@@ -10,6 +10,7 @@ int main(int argc, char **argv)
 	const long PTRSIZE = 16384;
 	unsigned long array[PTRSIZE];
 	unsigned long *p = array;
+	unsigned long *op;
 	unsigned long ptrNum = 0;
 	unsigned long *tempPtr = p;
 	memset(p, 0x00, PTRSIZE);
@@ -17,6 +18,7 @@ int main(int argc, char **argv)
 	int loop[4096] = {0};
 	int *l = loop;
 	int *tempLoop = l;
+	int *ol;
 
 	/* Files */
 	FILE *f = fopen(argv[1], "r");
@@ -85,15 +87,17 @@ int main(int argc, char **argv)
 			case '{':
 				tempPtr = p;
 				p = malloc(sizeof(long)*PTRSIZE);
+				op = p;
 				*p = *tempPtr;
 				tempLoop = l;
 				l = malloc(sizeof(int)*4096);
+				ol = l;
 				break;
 			case '}':
 				*tempPtr = *p;
-				free(p);
+				free(op);
 				p = tempPtr;
-				free(l);
+				free(ol);
 				l = tempLoop;
 				break;
 		}

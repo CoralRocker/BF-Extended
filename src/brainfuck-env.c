483e7d423b36b41e016531d1006ac60bbbccbb58
Gaultier Delbarre <gaultiertexas@gmail.com>
2019-10-18 20:20:56 -0500

    env doesn't work yet

--- src/brainfuck-env.c
+++ src/brainfuck-env.c
@@ -16,12 +16,12 @@ typedef struct scratchpad {
 int main(int argc, char **argv)
 {
 
-	char fname[LINE_MAX];
-	if(argc < 2){
-		printf("No filename given. Please enter name of file to interpret: ");
-		fgets(fname, LINE_MAX, stdin);
-		fname[strlen(fname)-1] = 0x00;
-	}
+	//char fname[LINE_MAX];
+	//if(argc < 2){
+	//	printf("No filename given. Please enter name of file to interpret: ");
+	//	fgets(fname, LINE_MAX, stdin);
+	//	fname[strlen(fname)-1] = 0x00;
+	//}
 
 
 	/* Pointer */
@@ -35,137 +35,143 @@ int main(int argc, char **argv)
 	vector *ScratchArr = initVector();
 
 	/* Files */
-	FILE *f = fopen(((argc>1)?argv[1]:fname), "r");
+	//FILE *f = fopen(((argc>1)?argv[1]:fname), "r");
 	
 	bool debug = false;
 	
 	/* Compile Loop */
-	char c;
+	char *line, c;
 	bool comment = false;
-	while((c = fgetc(f)) != EOF)
+	FILE *stream;
+	while(getline(line, 0, stdin) != -1)
 	{
-		switch(c)
+		stream = fmemopen(line, strlen(line), "r");
+		while((c = fgetc(stream)) != EOF)
 		{
-			case '/':
-			{
-				uint32_t temp = ftell(f);
-				c = fgetc(f);
-				if(c == '*')
-					comment = true;
-				else
-					fseek(f, temp, SEEK_SET);
-				break;
-			}
-			case '*':
-			{
-				uint32_t temp = ftell(f);
-				c = fgetc(f);
-				if(c == '/')
-					comment = false;
-				else
-					fseek(f, temp, SEEK_SET);
-				break;
-			}
-		}
-		if(!comment){
 			switch(c)
 			{
-				case '>':
-					if(bfArrPos + 1 < bfArrSize)
-					{
-						bfArrPos++;
-					}else{
-						pushBackVector(bfArray, 0);
-						bfArrSize++;
-						bfArrPos++;
-					}
-					break;
-
-				case '<':
-					if(bfArrPos != 0)
-					{
-						bfArrPos--;
-					}
-					break;
-
-				case '+':
-					assignVector(bfArray, bfArrPos, atVector(bfArray, bfArrPos) + 1);
-					break;
-
-				case '-':
-					if(atVector(bfArray, bfArrPos) <= 0)
-						assignVector(bfArray, bfArrPos, 0);
+				case '/':
+				{
+					uint32_t temp = ftell(stream);
+					c = fgetc(stream);
+					if(c == '*')
+						comment = true;
 					else
-						assignVector(bfArray, bfArrPos, atVector(bfArray, bfArrPos) - 1);
+						fseek(stream, temp, SEEK_SET);
 					break;
-
-				case '.':
-					if(!debug)
-						printf("%c", (int) atVector(bfArray, bfArrPos));
+				}
+				case '*':
+				{
+					uint32_t temp = ftell(stream);
+					c = fgetc(stream);
+					if(c == '/')
+						comment = false;
 					else
-						printf("%d\n", (int) atVector(bfArray, bfArrPos));
+						fseek(stream, temp, SEEK_SET);
 					break;
-
-				case ',':
-					assignVector(bfArray, bfArrPos, (void*) getchar());
-					break;
-
-				case '[':
-					pushBackVector(bfLoop, (void*) ftell(f));
-					bfLpPos++;
-					break;
-
-				case ']':
-					if(atVector(bfArray, bfArrPos) == 0)
-					{	
-						popBackVector(bfLoop);
-					}else{
-						fseek(f, (long int) backVector(bfLoop), SEEK_SET);
-					}
-					break;
-
-				case '{':
-					{
-					scratchpad *temp = malloc(sizeof(scratchpad));
-					temp->arrPtr = initVector();
-					temp->loopPtr = initVector();
-					temp->prevArr = bfArray;
-					temp->prevLoop = bfLoop;
-					temp->prevArrSize = bfArrSize;
-					temp->prevArrPos = bfArrPos;
-					temp->prevLoopPos = bfLpPos;
-					
-					bfArray = temp->arrPtr;
-					bfLoop = temp->loopPtr;
-					bfArrSize = 1;
-					bfArrPos = 0;
-					bfLpPos = 0;
-					pushBackVector(bfArray, atVector(temp->prevArr, temp->prevArrPos));
-					pushBackVector(ScratchArr, temp);
-					break;
-					}
-				case '}':
-					{
-					scratchpad *temp = popBackVector(ScratchArr);
-					bfArray = temp->prevArr;
-					bfLoop = temp->prevLoop;
-					assignVector(bfArray, temp->prevArrPos, atVector(temp->arrPtr, bfArrPos));
-					freeVector(temp->arrPtr);
-					freeVector(temp->loopPtr);
-					bfArrSize = temp->prevArrSize;
-					bfArrPos = temp->prevArrPos;
-					bfLpPos = temp->prevLoopPos;
-					free(temp);
-					break;
-					}
+				}
+			}
+			if(!comment){
+				switch(c)
+				{
+					case '>':
+						if(bfArrPos + 1 < bfArrSize)
+						{
+							bfArrPos++;
+						}else{
+							pushBackVector(bfArray, 0);
+							bfArrSize++;
+							bfArrPos++;
+						}
+						break;
+	
+					case '<':
+						if(bfArrPos != 0)
+						{
+							bfArrPos--;
+						}
+						break;
+	
+					case '+':
+						assignVector(bfArray, bfArrPos, atVector(bfArray, bfArrPos) + 1);
+						break;
+	
+					case '-':
+						if(atVector(bfArray, bfArrPos) <= 0)
+							assignVector(bfArray, bfArrPos, 0);
+						else
+							assignVector(bfArray, bfArrPos, atVector(bfArray, bfArrPos) - 1);
+						break;
+	
+					case '.':
+						if(!debug)
+							printf("%c", (int) atVector(bfArray, bfArrPos));
+						else
+							printf("%d\n", (int) atVector(bfArray, bfArrPos));
+						break;
+	
+					case ',':
+						assignVector(bfArray, bfArrPos, (void*) getchar());
+						break;
+	
+					case '[':
+						pushBackVector(bfLoop, (void*) ftell(stream));
+						bfLpPos++;
+						break;
+	
+					case ']':
+						if(atVector(bfArray, bfArrPos) == 0)
+						{	
+							popBackVector(bfLoop);
+						}else{
+							fseek(stream, (long int) backVector(bfLoop), SEEK_SET);
+						}
+						break;
+	
+					case '{':
+						{
+						scratchpad *temp = malloc(sizeof(scratchpad));
+						temp->arrPtr = initVector();
+						temp->loopPtr = initVector();
+						temp->prevArr = bfArray;
+						temp->prevLoop = bfLoop;
+						temp->prevArrSize = bfArrSize;
+						temp->prevArrPos = bfArrPos;
+						temp->prevLoopPos = bfLpPos;
+						
+						bfArray = temp->arrPtr;
+						bfLoop = temp->loopPtr;
+						bfArrSize = 1;
+						bfArrPos = 0;
+						bfLpPos = 0;
+						pushBackVector(bfArray, atVector(temp->prevArr, temp->prevArrPos));
+						pushBackVector(ScratchArr, temp);
+						break;
+						}
+					case '}':
+						{
+						scratchpad *temp = popBackVector(ScratchArr);
+						bfArray = temp->prevArr;
+						bfLoop = temp->prevLoop;
+						assignVector(bfArray, temp->prevArrPos, atVector(temp->arrPtr, bfArrPos));
+						freeVector(temp->arrPtr);
+						freeVector(temp->loopPtr);
+						bfArrSize = temp->prevArrSize;
+						bfArrPos = temp->prevArrPos;
+						bfLpPos = temp->prevLoopPos;
+						free(temp);
+						break;
+						}
+				}
 			}
 		}
+		fclose(stream);
+		free(line);
 	}
-
 	freeVector(bfArray);
 	freeVector(bfLoop);
 	freeVector(ScratchArr);
 	printf("\n");
 	/* Close Files */
-	fclose(f);
+	//fclose(f);
 }	
